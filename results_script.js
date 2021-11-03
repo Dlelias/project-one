2882966f6ef68d5304e4d279aff30445eab676a0
Mason Newell <mason.newell13@gmail.com>
2021-11-02 18:54:50 -0700

    changed results to dynamically render

---
+++ results_script.js
@@ -0,0 +1,25 @@
+var cardImageEl = "url for property image";
+var cardHeaderEl = "Address";
+var cardTextEl = "Price/size";
+
+function tester() {
+  for (var i = 0; i < 8; i++) {
+    $(".card-container").append(`<div class="col-12 col-xl-3 col-md-5 p-5" id="card-8">
+    <div class="card">
+      <img src="https://via.placeholder.com/150" class="card-img-top" alt="..." />
+      <div class="card-body">
+        <h5 class="card-header">${cardHeaderEl}</h5>
+        <p class="card-text">
+          ${cardTextEl}
+        </p>
+        <a href="#" class="btn btn-primary myButton">Select Property</a>
+      </div>
+    </div>
+  </div>`);
+  }
+}
+
+// Event-listener (button pressed)
+$(".card-container").on("click", ".myButton", function (event) {});
+
+tester();
